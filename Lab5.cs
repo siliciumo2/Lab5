593df157ecde67f327ba1dc1f99692f6f73fb31f
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2023-10-30 13:25:25 +0300

    Add files via upload

---
+++ Lab5.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите размерность массива");
+int raz=Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[raz];
+Console.WriteLine("Введите элементы массива");
+for (int i = 0; i < raz; i++)
+{
+    mass[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < raz; i++)
+{
+    Console.Write($" {mass[i]}");
+}
+{
+    for (int i = 1; i < raz; i += 2)
+    {
+        int temp = mass[i];
+        mass[i] = mass[i-1];
+        mass[i-1] = temp;
+    }
+}
+for(int i = 0; i < raz; i++)
+{
+    Console.Write($" {mass[i]}");
+}
